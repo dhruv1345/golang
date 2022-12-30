f7eb20ce8c39cdd6c9d2ebfd63eb7bf9d96f91aa
dhruv1345 <21102156@mail.jiit.ac.in>
2022-12-30 22:23:35 +0530

    after this we will be adding user input states

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Print("Welcome to our conference booking application")
-	fmt.Print("\nGet your tickets here !!!")
+	conferenceName := "Go Conference"
+	const conferencetickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("\nCoferenceTicket is %T, remainingTickets is %T, conferenceName is %T\n", conferencetickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application \n", conferenceName)
+	fmt.Println("We have total of", conferencetickets, "tickets and ", remainingTickets, " are still available")
+	fmt.Print("\nGet your tickets here !!!\n")
+
+	var userName string
+	var usertickets int
+
+	userName = "Tom"
+	usertickets = 2
+
+	fmt.Printf("User %v booked %v tickets.\n\n", userName, usertickets)
+
 }
